1bb87a33fc8d90a8c8eb88cc13828c68f1f48f22
saganorth <saga.nord99@hotmail.se>
2025-09-11 10:48:29 +0200

    ana

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/next';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
